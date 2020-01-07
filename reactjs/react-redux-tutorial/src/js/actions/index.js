5d2c0f7949f6e1a93d813fd2fe1cf1c7df9d51a1
Alen Malhasoglu <AlenMalhasoglu@hotmail.com>
2020-01-07 18:36:13 +0000

    added constants

--- reactjs/react-redux-tutorial/src/js/actions/index.js
+++ reactjs/react-redux-tutorial/src/js/actions/index.js
@@ -1,8 +1,10 @@
 // src/js/actions/index.js
 
+import {ADD_ARTICLE} from "../constants/action-types";
+
 export function addArticle(payload){
     return {
-        type: "ADD_ARTICLE", 
+        type: ADD_ARTICLE, 
         payload
     }
 };
