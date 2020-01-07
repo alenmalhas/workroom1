1db97451c951e40b2c416b6cf948a41a9ebe2cef
Alen Malhasoglu <AlenMalhasoglu@hotmail.com>
2020-01-07 18:57:26 +0000

    updated react-redux-tutorial/src/index.js

--- reactjs/react-redux-tutorial/src/js/reducers/index.js
+++ reactjs/react-redux-tutorial/src/js/reducers/index.js
@@ -1,8 +1,19 @@
+import { ADD_ARTICLE } from "../constants/action-types"
+
 const initialState = {
     articles : []
 };
 
 function rootReducer(state = initialState, action) {
+
+    if(action.type == ADD_ARTICLE){
+        var newState = Object.assign({}, state, {
+            articles: stete.articles.concat(action.payload)
+        });
+        //state.articles.push(action.payload);
+        return newState;
+    }
+
     return state;
 };
 
