e996aa666f08159ba022ca22569874d18a314c3c
Alen Malhasoglu <AlenMalhasoglu@hotmail.com>
2020-12-07 12:17:58 +0000

    updated SockMerchantTests

--- hackerRank/warm-up-challenges/SalesByMatch/SalesByMatch.PerformanceTests/SockMerchantTests.cs
+++ hackerRank/warm-up-challenges/SalesByMatch/SalesByMatch.PerformanceTests/SockMerchantTests.cs
@@ -37,25 +37,32 @@ namespace SalesByMatch.PerformanceTests
     }
 
     /*
-    public class Md5VsSha256
+[SimpleJob(RuntimeMoniker.Net472, baseline: true)]
+[SimpleJob(RuntimeMoniker.NetCoreApp30)]
+[SimpleJob(RuntimeMoniker.CoreRt30)]
+[SimpleJob(RuntimeMoniker.Mono)]
+[RPlotExporter]
+public class Md5VsSha256
+{
+    private SHA256 sha256 = SHA256.Create();
+    private MD5 md5 = MD5.Create();
+    private byte[] data;
+
+    [Params(1000, 10000)]
+    public int N;
+
+    [GlobalSetup]
+    public void Setup()
     {
-        private const int N = 10000;
-        private readonly byte[] data;
-
-        private readonly SHA256 sha256 = SHA256.Create();
-        private readonly MD5 md5 = MD5.Create();
-
-        public Md5VsSha256()
-        {
-            data = new byte[N];
-            new Random(42).NextBytes(data);
-        }
-
-        [Benchmark]
-        public byte[] Sha256() => sha256.ComputeHash(data);
-
-        [Benchmark]
-        public byte[] Md5() => md5.ComputeHash(data);
+        data = new byte[N];
+        new Random(42).NextBytes(data);
     }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+}
     */
 }
