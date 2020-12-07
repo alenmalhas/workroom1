8a00ba4d4fd6d188a600c03fbcd4adb0eb64b102
Alen Malhasoglu <AlenMalhasoglu@hotmail.com>
2020-12-07 11:32:13 +0000

    Added SalesByMatch and SalesByMatch.UnitTests

---
+++ hackerRank/warm-up-challenges/SalesByMatch/SalesByMatch.UnitTests/ProgramTests.cs
@@ -0,0 +1,30 @@
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace SalesByMatch.UnitTests
+{
+    public class ProgramTests
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 1 }, 1)]
+        [InlineData(new[] { 1, 2, 3 }, 0)]
+        public void Test_sockMerchant(int[] sockIdArray, int expectedPairCount)
+        {
+            //arrange
+
+            //act
+            var pairCount = Program.sockMerchant(sockIdArray.Length, sockIdArray);
+
+            //assert
+            pairCount.Should().Be(expectedPairCount);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            true.Should().Be(true);
+
+        }
+    }
+}
---
+++ hackerRank/warm-up-challenges/SalesByMatch/SalesByMatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    public class Program
+    {
+        // Complete the sockMerchant function below.
+        public static int sockMerchant(int n, int[] ar)
+        {
+            var queryPairCount = from oneSock in ar
+                                 group oneSock by oneSock into groupSocks
+                                 where groupSocks.Count() % 2 == 0
+                                 select new { sockId = groupSocks.Key, count = groupSocks.Count() };
+            var list1 = queryPairCount.ToList();
+            return list1.Count;
+        }
+
+        public static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(
+                Console.ReadLine().Split(' '),
+                arTemp => Convert.ToInt32(arTemp)
+                );
+            int result = sockMerchant(n, ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
